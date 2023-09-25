dffe47e0b931ec9fff82d8bb33d95c4e32f41c14
Yin Yin Wu-Hanke <y.wu-hanke@ellusion.de>
2023-09-25 14:11:26 +0200

    content

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,14 +1,31 @@
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>About us</title>
+	<meta name="description" content="About us" />
 </svelte:head>
 
 <h1>About us</h1>
 
-
+<div>
+	<p>
+		Unsere Kanzlei zeichnet sich durch eine dynamische Struktur aus. Mit fundiertem Fachwissen und Leidenschaft für die Prüfung und Bewertung von Unternehmen in verschiedenen Wirtschaftszweigen sind wir Ihr verlässlicher Ansprechpartner.
+	</p>
+	<p>
+		Unsere langjährige Erfahrung, fundierten Fachkenntnisse und spezialisierten Branchenkenntnisse in den Bereichen Kredit-, Finanzdienstleistungs- und Wertpapierinstituten sowie Leasinggesellschaften qualifizieren uns als Ihren professionellen Partner auf höchstem Niveau.
+	</p>
+	<p>
+		Wir verstehen die spezifischen Anforderungen und regulatorischen Herausforderungen, denen unsere Mandanten gegenüberstehen, und bieten maßgeschneiderte Lösungen, die ihnen helfen, ihre Ziele zu erreichen.
+	</p>
+	<p>
+		Unser Engagement für Exzellenz spiegelt sich in der Qualität unserer Arbeit wider. Wir sind stolz darauf, unseren Mandanten durch fundierte Analysen, klare Kommunikation und effiziente Prozesse zu unterstützen.
+	</p>
+</div>
 
 <style lang="scss">
-	h1 {
-		color: blue;
+	div {
+		width: 80%;
+		margin-left: auto;
+		margin-right: auto;
+		background-color: #00558e; // ToDo variable!
+		padding: 2rem;
 	}
 </style>
