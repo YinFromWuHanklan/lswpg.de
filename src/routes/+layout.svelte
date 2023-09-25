ce61904fc7b2208846e4cbfb487e45e62dc82c92
Yin Yin Wu-Hanke <y.wu-hanke@ellusion.de>
2023-09-25 15:40:41 +0200

    impressum

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>&copy; 2023 Linner Seidemann Wirtschaftsprüfung | <a href="/impressum">Impressum</a> | <a href="/datenschutz">Datenschutz</a></p>
 	</footer>
 </div>
 
---
+++ src/routes/datenschutz/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Datenschutz</title>
+	<meta name="description" content="Datenschutz" />
+</svelte:head>
+
+	<h1>Datenschutz</h1>
+	<p>To do</p>
