3388987b6025f7cf09dfe39f8e7002cd9646c002
Yin Yin Wu-Hanke <y.wu-hanke@ellusion.de>
2023-10-06 15:00:15 +0200

    navi mobile

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -116,7 +116,7 @@
 		border-radius: 5px;
 
 		@media (max-width: 672px) {
-			width: fit-content;
+			width: auto;
 			margin: 0.8rem;
 		}
 
@@ -161,7 +161,7 @@
 			}
 
 			@media (max-width: 672px) {
-				width: fit-content;
+				width: auto;
 				padding: 1.8rem 1rem 1rem;
 
 				.service_content {
